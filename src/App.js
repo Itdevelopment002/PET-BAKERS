a790f553442624b1a99d9a5b5abe2d431b40d1f0
Abhay31 <abhaysharma0983@gmail.com>
2024-12-18 12:33:18 +0530

    new_chngs_Abhay

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
-// import './App.css';
 import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
 import Contact from './components/Contact/Contact';
@@ -10,15 +10,20 @@ import Home from './components/Home/Home';
 
 function App() {
   return (
-    <>
-    <Header />
-    {/* <AboutUs /> */}
-    {/* <Contact /> */}
-    {/* <Faq /> */}
-    {/* <PrivacyPolicy /> */}
-    <Home />
-    <Footer />
-    </>
+    <Router>
+      <Header />
+      <main style={{ minHeight: '80vh' }}>
+        <Routes>
+          {/* Default route to Home */}
+          <Route path="/" element={<Navigate to="/home" />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/pages/contact" element={<Contact />} />
+          <Route path="/pages/faqs" element={<Faq />} />
+          <Route path="/policies/privacy-policy" element={<PrivacyPolicy />} />
+        </Routes>
+      </main>
+      <Footer />
+    </Router>
   );
 }
 
